ff1ee2fd5af7ab54d159fd02039378c3060359dd
Anna Kesova <IZABELE-TAN4@yandex.ru>
2022-10-05 23:00:14 +0300

    ревью3

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -12,13 +12,13 @@ function SearchForm() {
         <input
           className="searchForm__linesearch"
           placeholder="Фильм"
-          required=""
+          required
         ></input>
         <button className="searchForm__search">Найти</button>
       </form>
       <div className="searhForm__radio filterCheckbox">
         <label className="filterCheckbox__radioButton">
-            <input className="filterCheckbox__input" required="" checked={isCheckbox} type="checkbox"
+            <input className="filterCheckbox__input" required checked={isCheckbox} type="checkbox"
         onChange={onCheckboxToggle}>
             </input>
             <span className="filterCheckbox__slider"></span> </label>
