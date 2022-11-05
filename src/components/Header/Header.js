9e549b23985bc30cc0070c8c5623634045ea371a
Anna Kesova <IZABELE-TAN4@yandex.ru>
2022-11-05 13:10:57 +0300

    header.Color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Header.css";
 import logo from "../../images/logo.svg";
 import HeaderMain from "./HeaderMain/HeaderMain";
 import HeaderMovies from "./HeaderMovies/HeaderMovies";
 
 function Header({ loggedIn }) {
+  const { pathname } = useLocation();
   return (
     <>
-      {
-        <header className={`page__header header ${loggedIn? "headerm" : "header"}`}>
-          <div className="header__content">
-            <Link className="header__link" to="/">
-              <img src={logo} className="header__logo" alt="logo" />
-            </Link>
-            {loggedIn ? <HeaderMovies /> : <HeaderMain />}
-          </div>
-        </header>
-      }
+      <header
+        className={`page__header ${pathname === "/" ? "header" : "headerm"} `}
+      >
+        <div className="header__content">
+          <Link className="header__link" to="/">
+            <img src={logo} className="header__logo" alt="logo" />
+          </Link>
+          {loggedIn ? <HeaderMovies /> : <HeaderMain />}
+        </div>
+      </header>
     </>
   );
 }
