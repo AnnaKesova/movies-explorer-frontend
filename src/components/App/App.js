2304a7128cf6f2dcf7a026bfaf1e3e16f4e295c3
Anna Kesova <IZABELE-TAN4@yandex.ru>
2023-04-27 14:35:33 +0300

    notfaund

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -190,7 +190,7 @@ function App() {
                 </>
               }
             />
-            <Route path="/*" element={<NotFound />} />
+            {/* <Route path="/*" element={<NotFound />} /> */}
             <Route
               path="/movies"
               element={
